dc657b1a070471fe4dcbab2b38e193bedfad64d7
Stass (JYS) <jyslearn@gmail.com>
2026-08-31 13:47:52 +0200

    Add TriLedger finance offline worker

---
+++ finance/sw.js
@@ -0,0 +1,1 @@
+const C='triledger-finance-v1';const F=['./','./index.html','./manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(F))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
